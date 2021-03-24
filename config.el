11fd9b6a5aa047c03bf635d7a3e17941b68835e9
AbstProcDo <abst.proc.do@qq.com>
2021-03-24 09:04:33 +0800

    2021-03-24 Wednesday

--- config.el
+++ config.el
@@ -226,40 +226,8 @@
 (setq diary-file "~/Documents/OrgMode/ORG/Master/standard-diary")
 
 
-
-;;Sunrise and Sunset
-;;日出而作, 日落而息
-(defun diary-sunrise ()
-  (let ((dss (diary-sunrise-sunset)))
-    (with-temp-buffer
-      (insert dss)
-      (goto-char (point-min))
-      (while (re-search-forward " ([^)]*)" nil t)
-	(replace-match "" nil nil))
-      (goto-char (point-min))
-      (search-forward ",")
-      (buffer-substring (point-min) (match-beginning 0)))))
-
-;; 日落而息
-(defun diary-sunset ()
-  (let ((dss (diary-sunrise-sunset))
-	start end)
-    (with-temp-buffer
-      (insert dss)
-      (goto-char (point-min))
-      (while (re-search-forward " ([^)]*)" nil t)
-	(replace-match "" nil nil))
-      (goto-char (point-min))
-      (search-forward ", ")
-      (setq start (match-end 0))
-      (search-forward " at")
-      (setq end (match-beginning 0))
-      (goto-char start)
-      (capitalize-word 1)
-            (buffer-substring start end))))
-
-
-;;Sunrise and Sunset
+;; Sunrise and sunset in agenda
+;;Sunrise
 ;;日出而作, 日落而息
 (defun diary-sunrise ()
   (let ((dss (diary-sunrise-sunset)))
